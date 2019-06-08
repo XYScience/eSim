9b7de49cdddf21b9cb34520b5ed2c5d115902b2a
SScience <chentushen.science@gmail.com>
2019-06-08 12:24:45 +0800

    add deleteSubscription

--- app/src/main/java/com/sscience/esim/MainActivity.kt
+++ app/src/main/java/com/sscience/esim/MainActivity.kt
@@ -1,33 +1,37 @@
 package com.sscience.esim
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.telephony.SubscriptionManager
-import android.util.Log
-import android.telephony.euicc.EuiccManager
-import android.R.attr.action
 import android.annotation.SuppressLint
 import android.app.PendingIntent
-import android.content.Intent
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.telephony.SubscriptionManager
 import android.telephony.TelephonyManager
 import android.telephony.euicc.DownloadableSubscription
+import android.telephony.euicc.EuiccManager
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import java.security.MessageDigest
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        val ACTIVATION_CODE = "LPA:1\$rsp.truphone.com$57F9937E8B4C4F77A48D8B449C60BAA4"
+        const val TAG = "MainActivity>>>"
+        val ACTIVATION_CODE = "LPA:1\$rsp.truphone.com\$2T-1GW21-1MEX2U4"
         const val ACTION_DOWNLOAD_SUBSCRIPTION = "download_subscription"
         const val ACTION_SWITCH_TO_SUBSCRIPTION = "switch_to_subscription"
+        const val ACTION_DELETE_SUBSCRIPTION = "delete_subscription"
     }
 
     private var euiccManager: EuiccManager? = null
-    private var callBackIntentDownload: PendingIntent? = null
-    private var callBackIntentSwitch: PendingIntent? = null
+    private var callBackIntent: PendingIntent? = null
 
     @SuppressLint("WrongConstant", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,59 +44,86 @@ class MainActivity : AppCompatActivity() {
         euiccManager = getSystemService(Context.EUICC_SERVICE) as EuiccManager
 
         val isEnable = euiccManager?.isEnabled
-        Log.e(">>>>>>>>>", "isEnable: $isEnable")
+        Log.e(TAG, "isEnable: $isEnable")
+        try {
+            val eid = euiccManager?.eid
+            Log.e(TAG, "eid: $eid")
+        } catch (e: Exception) {
+            Log.e(TAG, ">>>>>: $e")
+        }
 
-        registerReceiver(receiverDownload, IntentFilter(ACTION_DOWNLOAD_SUBSCRIPTION))
-        registerReceiver(receiverSwitch, IntentFilter(ACTION_SWITCH_TO_SUBSCRIPTION))
+        registerReceiver(receiver, IntentFilter(ACTION_DOWNLOAD_SUBSCRIPTION))
+        registerReceiver(receiver, IntentFilter(ACTION_SWITCH_TO_SUBSCRIPTION))
+        registerReceiver(receiver, IntentFilter(ACTION_DELETE_SUBSCRIPTION))
+
 
         btn_download.setOnClickListener {
+            Log.e(TAG, "onCLick: $ACTION_DOWNLOAD_SUBSCRIPTION")
             val intent = Intent(ACTION_DOWNLOAD_SUBSCRIPTION)
-            callBackIntentDownload = PendingIntent.getBroadcast(this, 10, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            callBackIntent = PendingIntent.getBroadcast(this, 10, intent, PendingIntent.FLAG_UPDATE_CURRENT)
             val sub: DownloadableSubscription = DownloadableSubscription.forActivationCode(ACTIVATION_CODE)
-            euiccManager?.downloadSubscription(sub, true, callBackIntentDownload)
+            Log.e(TAG, "ac: ${sub.encodedActivationCode}")
+            euiccManager?.downloadSubscription(sub, true, callBackIntent)
         }
 
         btn_switch.setOnClickListener {
+            Log.e(TAG, "onCLick: $ACTION_SWITCH_TO_SUBSCRIPTION")
             val intent = Intent(ACTION_SWITCH_TO_SUBSCRIPTION)
-            callBackIntentSwitch = PendingIntent.getBroadcast(this, 12, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-            euiccManager?.switchToSubscription(10, callBackIntentSwitch)
+            callBackIntent = PendingIntent.getBroadcast(this, 10, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val subscriptionId: String = et_switch_subscription_id.text.toString()
+            euiccManager?.switchToSubscription(subscriptionId.toInt(), callBackIntent)
         }
+
+        btn_delete.setOnClickListener {
+            Log.e(TAG, "onCLick: $ACTION_DELETE_SUBSCRIPTION")
+            val intent = Intent(ACTION_DELETE_SUBSCRIPTION)
+            callBackIntent = PendingIntent.getBroadcast(this, 10, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+            val subscriptionId: String = et_delete_subscription_id.text.toString()
+            euiccManager?.deleteSubscription(subscriptionId.toInt(), callBackIntent)
+        }
+
+        val tm: TelephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+        val cp = tm.hasCarrierPrivileges()
+        Log.e(TAG, "hasCarrierPrivileges: $cp")
+
+        val packageInfo: PackageInfo
+        try {
+            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for (signature in packageInfo.signatures) {
+                val md = MessageDigest.getInstance("SHA-1")
+                val certHash: ByteArray = md.digest(signature.toByteArray())
+                Log.e(">>>>>>>>>", "signature hash: ${Arrays.toString(certHash)}")
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+            throw IllegalArgumentException("Unknown package: $packageName", e)
+        }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        Log.e(">>>>>>>>>", "requestCode: $requestCode, resultCode: $resultCode")
+        Log.e(TAG, "onActivityResult requestCode: $requestCode, resultCode: $resultCode")
     }
 
-    private var receiverDownload: BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            if (ACTION_DOWNLOAD_SUBSCRIPTION != intent.action) {
-                return
-            }
+    private val receiver: BroadcastReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent == null) return
+            if (ACTION_DOWNLOAD_SUBSCRIPTION != intent.action
+                && ACTION_SWITCH_TO_SUBSCRIPTION != intent.action
+                && ACTION_DELETE_SUBSCRIPTION != intent.action
+            ) return
+
+            Log.e(TAG, "receiver: ${intent.action}")
             val detailedCode = intent.getIntExtra(
                 EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_DETAILED_CODE, -1 /* defaultValue*/
             )
             val resultCode = resultCode
             if (resultCode == EuiccManager.EMBEDDED_SUBSCRIPTION_RESULT_RESOLVABLE_ERROR) {
-                euiccManager?.startResolutionActivity(this@MainActivity, 11, intent, callBackIntentDownload)
+                euiccManager?.startResolutionActivity(this@MainActivity, 11, intent, callBackIntent)
             }
-            Log.e(">>>>>>>>>", "receiverDownload detailedCode: $detailedCode, resultCode: $resultCode")
+            Log.e(TAG, "onReceive detailedCode: $detailedCode, resultCode: $resultCode")
         }
-    }
 
-    private var receiverSwitch: BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            if (ACTION_SWITCH_TO_SUBSCRIPTION != intent.action) {
-                return
-            }
-            val detailedCode = intent.getIntExtra(
-                EuiccManager.EXTRA_EMBEDDED_SUBSCRIPTION_DETAILED_CODE, -1 /* defaultValue*/
-            )
-            val resultCode = resultCode
-            if (resultCode == EuiccManager.EMBEDDED_SUBSCRIPTION_RESULT_RESOLVABLE_ERROR) {
-                euiccManager?.startResolutionActivity(this@MainActivity, 11, intent, callBackIntentSwitch)
-            }
-            Log.e(">>>>>>>>>", "receiverSwitch detailedCode: $detailedCode, resultCode: $resultCode")
-        }
     }
 }
